1d3dad3641a21bd9f0cc206b10ecf810bf883e84
Horizon <55484667+Horizon221@users.noreply.github.com>
2020-07-23 19:51:05 +0800

    Add files via upload

---
+++ Auto_Rename-master/Auto_Rename/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Auto_Rename
+{
+    class Program
+    {
+        public static string[] name;
+        static void Main(string[] args)
+        {
+            string Path = AppDomain.CurrentDomain.BaseDirectory;
+            StreamReader sr = new StreamReader(Path + "/名单.txt", Encoding.UTF8);
+            string content;
+            while ((content = sr.ReadLine()) != null)
+            {
+                name = content.Split(',');
+            }
+            for (int i = 0; i < name.Length; i++)
+            {
+                int s = i + 1;
+                if (File.Exists(Path + "/" + name[i] + ".png"))
+                {
+                    File.Move(Path + "/" + name[i] + ".png", Path + "/" + s + " " + name[i] + ".png");
+                }
+                else if (File.Exists(Path + "/" + name[i] + ".jpg"))
+                {
+                    File.Move(Path + "/" + name[i] + ".jpg", Path + "/" + s + " " + name[i] + ".jpg");
+                }
+            }
+        }
+    }
+}
